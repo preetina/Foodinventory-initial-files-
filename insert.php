e46d66fbad14a309d67756c0018777d076981749
Preetina Shakya <sakyapreetina1@gmail.com>
2017-06-06 20:08:15 +0545

    interfacefor inserting an item into database

--- insert.php
+++ insert.php
@@ -1,4 +1,10 @@
 <?php
+session_start();
+//check if user is logged in.If not redirect to admin page.
+if(!isset($_SESSION['admin']))
+{
+	//header("Location:index.php?page=admin");
+}
 include("index.php");
 include("dbconnect.php");
 ?>
@@ -12,25 +18,26 @@ include("dbconnect.php");
     <h1 style="text-align: left; font-family: Lucida Calligraphy; font-size:19px;"><b><u>Admin Panel</u></b></h1>
 <br />
 <p style="font-size: 17px; font-family:Minion Pro Med;"><a href="home.php"><b>Logout</b></a></p>
+<p style="font-size: 17px; font-family:Minion Pro Med;"><a href="insert.php" ><b>Add Stock</b></a></p>
 
-<p style="font-size: 17px; font-family:Minion Pro Med;"><a href="insert.php" ><b>Stock Management</b></a></p>
 
-<p style="font-size:17px; font-family: Minion Pro Med;"><a href="home.php"><b>Foodlist Management</b> </a></p>
+<p style="font-size: 17px; font-family:Minion Pro Med;"><a href="stock_modify.php" ><b>Update Stock</b></a></p>
+<p style="font-size: 17px; font-family:Minion Pro Med;"><a href="delete_item.php" ><b>Delete Stock</b></a></p>
+
+<p style="font-size:17px; font-family: Minion Pro Med;"><a href="menupanel.php"><b>Menu Management</b> </a></p>
     
 </div>
 <br/>
-<form action="add.php" method="post">
+<form action="insert_item.php" method="post">
 <p><b>Manage your stock here.</b></p>
-<input type="text" name="Item" placeholder="Item" ><br/><br/>
-<input type="text" name="Quantity" placeholder="Quantity" ><br/></br>
-<input type="text" name="Rate" placeholder="Rate"><br/></br>
-<input type="text" name="Date" placeholder="Date">
+<br/><input type="text" name="Item" placeholder="Item" ><br/><br/>
+<br/><input type="text" name="Quantity" placeholder="Quantity" ><br/></br>
+<br/><input type="text" name="Date" placeholder="Date"><br/></br>
+<br/><input type="text" name="Rate" placeholder="Rate">
+
 <div>
 <br/><br/>
 <input type="submit" name="submit" value="Add">
-<input type="button" name="insert" value="Update">
-<input type="button" name="insert" value="Delete">
-<input type="button" name="insert" value="Find">
 
 </div>
 </div>
@@ -39,4 +46,4 @@ include("dbconnect.php");
 
 </body>
 </html>
-<?
+
